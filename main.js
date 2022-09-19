27bba6f1138dd540a4b739bf66792656cb454c9a
Florian PASTEUR <florian.c.p@pm.me>
2022-09-19 18:08:07 +0200

    Change default settings instead

--- main.js
+++ main.js
@@ -1,56 +1,5 @@
 var gPrevValue = '';
-var gConfigData = {
-      bpxColor: true,
-      bpxIndent: true,
-      bpxAutoComplete: true,
-      bpxModuleAC: true,
-      bpxPropAC: true,
-      bpxGrid: true,
-      bpxTooltip: true,
-      bpxSnippet: true,
-      decimal_txt: 'COMMA',
-      thousands_txt: 'SPACE',
-      negative_txt: 'MINUS_SIGN',
-      zero_txt: 'HYPHEN',
-      bpxLink: true,
-      bpxMap: true,
-      bpxToaster: true,
-      bpxPalette: true,
-      bpxScrollbar: true,
-      bpxCellcalculation: true,
-      nav_back_color1: '#2B6ECB',
-      nav_back_color2: '#102161',
-      nav_button_color: '#2bb8ff',
-      nav_btntxt_color: '#ffffff',
-      slider_back_color2: '#f8f8fa',
-      slider_txt_color: '#333333',
-      settings_selected_color2: '#102161',
-      settings_hover_color: '#2bb8ff',
-      label_txt_color: '#2bb8ff',
-      editable_cells_color: '#fffed0',
-      editable_txt_color: '#0433FF',
-      sum1_cells_color: '#C5C5C5',
-      sum2_cells_color: '#E0E0E0',
-      sum3_cells_color: '#F4F4F4',
-      heading3_cells_color: '#F1F1F1',
-      sum1_col_bdr: '#cccccc',
-      sum2_col_bdr: '#cccccc',
-      h1_txt_color: '#102161',
-      h1_bg_color: '#ffffff',
-      h1_txt_align: 'left',
-      h2_txt_color: '#424242',
-      h2_bg_color: '#ffffff',
-      h2_txt_align: 'left',
-      info_bg_color: '#c4ebff',
-      blue_btn_color: '#194788',
-      blue_btn_hover: '#2bb8ff',
-      tbl_font_family: 'helvetica',
-      tbl_font_size: '11',
-      th_back_color: '#F1F1F1',
-      th_txt_color: '#000000',
-      ntf_version: 1
-    }
-;
+var gConfigData = {};
 
 //Initialize on opening
 applyToaster(gConfigData);
@@ -339,4 +288,4 @@ function readSettings() {
   });
 }
 
-// readSettings();
+readSettings();
